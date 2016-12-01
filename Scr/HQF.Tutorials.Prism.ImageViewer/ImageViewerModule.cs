e3f92d8bccb3164dc61e21bd1eb0a79c6c8242cb
huoxudong125 <423huo@163.com>
2016-12-02 00:03:27 +0800

    Add ImageView from command in menubar ,need fix.

--- Scr/HQF.Tutorials.Prism.ImageViewer/ImageViewerModule.cs
+++ Scr/HQF.Tutorials.Prism.ImageViewer/ImageViewerModule.cs
@@ -1,5 +1,9 @@
-﻿using HQF.Tutorials.Prism.Infrastructure.Constants;
+﻿using System.Windows;
+using HQF.Tutorials.Prism.Extensions.Menus;
+using HQF.Tutorials.Prism.ImageViewer.Views;
+using HQF.Tutorials.Prism.Infrastructure.Constants;
 using Microsoft.Practices.Unity;
+using Prism.Commands;
 using Prism.Modularity;
 using Prism.Regions;
 
@@ -9,19 +13,58 @@ namespace HQF.Tutorials.Prism.ImageViewer
     {
         private IRegionManager _regionManager;
         private readonly IUnityContainer _unityContainer;
+        private readonly IMenuService _menuService;
+        private ImageViewerView _view;
 
         public ImageViewerModule(RegionManager regionManager,
-            IUnityContainer unityContainer)
+            IUnityContainer unityContainer, IMenuService menuService)
         {
             _regionManager = regionManager;
             _unityContainer = unityContainer;
+            this._menuService = menuService;
         }
 
         public void Initialize()
         {
             _unityContainer.RegisterType<Views.ImageViewerView>();
-            _regionManager.RegisterViewWithRegion(RegionNames.ContentRegion, typeof(Views.ImageViewerView));
+           
+            if (_menuService != null)
+                AddMenuItems();
+            else
+                _regionManager.RegisterViewWithRegion(RegionNames.ContentRegion, typeof(Views.ImageViewerView));
+
         }
 
+        private void AddMenuItems()
+        {
+
+            // You would need to put useful commands in here too
+            _menuService.RegisterMenu(new MenuItemNode(DefaultTopLevelMenuNames.Tools)
+            {
+                Text = "Tool1",
+                Command = new DelegateCommand(ShowImageViewerView)
+            });
+
+            // I'll have to think about a way to ignore the mnemonic specifier '_' in the RegisterMenu method, probably just remove all _ before tokenization / comparison
+            _menuService.RegisterMenu(new MenuItemNode(DefaultTopLevelMenuNames.File + "/_Open")
+            {
+                Text = "_Project"
+            });
+        }
+
+        private void ShowImageViewerView()
+        {
+            if (_view == null)
+            {
+                _view=_unityContainer.Resolve<ImageViewerView>();
+                _view.HorizontalAlignment=HorizontalAlignment.Stretch;
+                _view.VerticalAlignment=VerticalAlignment.Stretch;
+                _view.Width = 800;
+                _view.Height = 600;
+                _regionManager.AddToRegion(RegionNames.ContentRegion, _view);
+            }
+
+            _regionManager.Regions[RegionNames.ContentRegion].Activate(_view);
+        }
     }
 }
